2195fb7780f0f46f2bf1e5da1e8806aec7a41d9f
Quedjo <quedjofrempong@gmail.com>
2023-05-12 10:08:52 +0000

    added read-details permission only for superuser

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import post
 from .serializers import postserializer
 
@@ -7,7 +7,11 @@ from .serializers import postserializer
 class postslist(generics.ListCreateAPIView):
     queryset=post.objects.all()
     serializer_class=postserializer
+    #permission_classes= (permissions.IsAuthenticatedOrReadOnly,)
+
 
 class postsdetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes= (permissions.IsAdminUser,)
     queryset=post.objects.all()
     serializer_class=postserializer
+    
